55a16955fcf17693448ff387d3f41e1f742436f3
freshhuk <freshhuk13@gmail.com>
2024-06-05 23:37:25 +0300

    Start writing cursach

---
+++ src/assignment12/TicketManager.java
@@ -0,0 +1,27 @@
+package assignment12;
+
+import java.util.Stack;
+
+class Ticket{
+    public String PIB;
+    public int flight_number;
+    public int price;
+    public String appointment;
+    public String flight_data;
+    public String flight_time;
+    public String flight_class;
+
+}
+public class TicketManager {
+    private Stack<Ticket> stack = new Stack<>();
+
+    public static void main(String[] args) {
+        //menu
+
+    }
+    private void addTicket(){
+        
+    }
+
+}
+
