f676b3754ac833122b2360e1ea375182b248be57
freshhuk <freshhuk13@gmail.com>
2024-06-18 00:05:14 +0300

    Create fisrt method

--- src/assignment13/TZforBAD.java
+++ src/assignment13/TZforBAD.java
@@ -4,13 +4,35 @@ import java.util.ArrayList;
 
 public class TZforBAD {
     public static void main(String[] args){
+        //true case
+        ArrayList<Integer> array = new ArrayList<>();
+        {
+            array.add(1);
+            array.add(9);
+            array.add(14);
+            array.add(19);
+            array.add(1);
+        }
+
+        int num = medianNumber(array);
+        System.out.println(num); // 11
 
     }
-
     private ArrayList<Integer> fileRead(){
         return null;
     }
     private void maxNumber(){
 
+    }
+    //true case , done
+    private static Integer medianNumber(ArrayList<Integer> array){
+        if(array.size() % 2 == 0){
+            int firstNum = array.get((array.size() / 2) - 1);
+            int secondNum = array.get(array.size() / 2);
+            return (firstNum + secondNum) / 2;
+        }
+        else{
+            return array.get(array.size() / 2);
+        }
     }
 }
